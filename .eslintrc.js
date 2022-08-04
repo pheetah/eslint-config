5f4f3b10d6d1858caef480ce1096ce5d09a8f4be
fatih ersoy <fatihersoy@Fatih-MacBook-Pro.local>
2022-08-04 14:13:17 +0300

    initial -f commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+        browser: true,
+        commonjs: true,
+        es2021: true
+    },
+    extends: "eslint:recommended",
+    parserOptions: {
+        ecmaVersion: "latest"
+    },
+    rules: {
+        "no-unused-vars": "off",
+        "multiline-ternary": ["error", "never"],
+        eqeqeq: ["error", "always"],
+        "arrow-spacing": ["error", { before: true, after: true }],
+        "function-call-argument-newline": ["error", "always"],
+        "func-call-spacing": ["error", "never"],
+        "computed-property-spacing": ["error", "never"],
+        "dot-location": ["error", "property"],
+        "no-new-object": "error",
+        "semi-spacing": ["error", { "before": false, "after": true }],
+        "space-before-blocks": ["error", "never"],
+        "space-before-function-paren": ["error", "never"],
+        "no-array-constructor": "error",
+        "camelcase": ["error", {"properties": "always", "ignoreDestructuring": true, "ignoreGlobals": true}],
+        "brace-style": ["error", "stroustrup"],
+        "eslint no-inline-comments": "error"
+    }
+};
---
+++ recommendeds.js
@@ -0,0 +1,17 @@
+const isOk = true;
+
+if(isOk){ 
+	alert("accepted"); 
+} 
+
+let cats = ["orange", "white", "black"];
+for(let i of cats){ 
+	alert("meow"); 
+}
+
+try{ 
+	console.log('looks good'); 
+} 
+catch(error){ 
+	console.log(error); 
+} 
